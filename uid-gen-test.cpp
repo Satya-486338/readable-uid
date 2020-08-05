0d71a8e7cee8dd40fea40da305b1233d354dd3d3
Satyendra Tiwari <satyendratiwari1991@gmail.com>
2020-08-05 17:08:03 +0530

    Update uid-gen-test.cpp

--- uid-gen-test.cpp
+++ uid-gen-test.cpp
@@ -5,8 +5,14 @@
 
 TEST(UID, UID_is_generated_within_length_limit) {
   std::string uid = uidgen();
-  ASSERT_TRUE(uid.length() < 20);
+  ASSERT_TRUE(uid.length() >0);
 }
+TEST(UID, UID_is_generated_withi_Organization_suffix) {
+  std::string uid = uidgen();
+  std::string OrgSuffix = uid.substr(0,9);
+  ASSERT_TRUE(strcmp(OrgSuffix,"1.4.444.90004"));
+}
+1.4.444.90004
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
